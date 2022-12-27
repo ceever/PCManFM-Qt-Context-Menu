16109d0b96af02c4caa1e13167412cf58d22807d
ceever <40247591+ceever@users.noreply.github.com>
2022-12-27 18:11:20 +0100

    Add files via upload

--- scripts/extract_here.sh
+++ scripts/extract_here.sh
@@ -6,7 +6,7 @@ for fo in "$@"
 do
 	if [ -f "$fo" ]
 	then
-		timeout -k 10 7z x -aou "$fo"
+		timeout -k 10 10 7z x -aou "$fo"
 		if [ 0 -ne $? ]
 		then
 			zenity --error --no-wrap --text="<b><big>Extraction failed for</big>\n\n<tt>$fo</tt></b>\n\n\nEither 7z took too long,\ngot stuck (password),\nor failed otherwise."
