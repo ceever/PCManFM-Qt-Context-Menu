e478d444ad4ce183a86e517564ed5749f334e300
ceever <40247591+ceever@users.noreply.github.com>
2023-03-04 19:25:46 +0000

    Enhanced with "... here" and "... subfolder" menu

--- scripts/extract_here.sh
+++ scripts/extract_here.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-cd "`dirname "$1"`"
 
+if [ "folder" == "$1" ]
+then
+	cd "`dirname "$2"`"
+else
+	cd "`dirname "$1"`"
+fi
 
 for fo in "$@"
 do
 	if [ -f "$fo" ]
 	then
-		timeout -k 10 10 7z x -aou "$fo"
+		echo "#$(basename "$fo")" | sed 'y/ /+/'
+		if [ "folder" == "$1" ]
+		then
+			mkdir -p "${fo%.*}"
+			timeout -k 10 10 7z x -aou -o"${fo%.*}" "$fo" > /dev/null
+		else
+			timeout -k 10 10 7z x -aou "$fo" > /dev/null
+		fi
 		if [ 0 -ne $? ]
 		then
-			zenity --error --no-wrap --text="<b><big>Extraction failed for</big>\n\n<tt>$fo</tt></b>\n\n\nEither 7z took too long,\ngot stuck (password),\nor failed otherwise."
+			zenity --error --no-wrap --text="<b><big>Extraction failed for</big>\n\n<tt>$fo</tt></b>\n\n\nEither 7z took too long,\ngot stuck (password),\nor failed otherwise." &
 		fi
+		sleep 1
 	fi
-done
+done | zenity --progress --pulsate --auto-close --title="Extracting ..." --text="..."
